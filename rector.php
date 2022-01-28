292d10f7879dbb62019a9270d0d90991009b6ec0
Ben Challis <ben-challis@users.noreply.github.com>
2022-01-28 21:03:28 +0000

    Use LevelSetList::UP_TO_PHP_80 @ Rector

--- rector.php
+++ rector.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\SetList;
+use Rector\Set\ValueObject\LevelSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -15,6 +16,5 @@ return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters->set(Option::PARALLEL, true);
 
     $containerConfigurator->import(SetList::CODE_QUALITY);
-    $containerConfigurator->import(SetList::PHP_74);
-    $containerConfigurator->import(SetList::PHP_80);
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_80);
 };
