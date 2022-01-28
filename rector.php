3b891e6dbb1deccfcbc452f673b02d15799dd3ed
Ben Challis <ben-challis@users.noreply.github.com>
2022-01-28 21:04:01 +0000

    Remove unused import

--- rector.php
+++ rector.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
-use Rector\Set\ValueObject\SetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
